4ad3a24dc8c0a9b9ccc5525a8fb8fd836970c65e
jalenng <jalen_ng@outlook.com>
2022-05-15 15:40:17 -0700

    Begin merging work; reorganize files

--- Assets/Flocking/Flock.cs
+++ Assets/Flocking/Flock.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
+    [SerializeField] float numberOfBirds = 50;
+    [SerializeField] GameObject birdPrefab;
+
     List<Bird> birds;
 
     [SerializeField] public float maxforce = 2f;    // Maximum steering force
@@ -13,25 +16,52 @@ public class Flock : MonoBehaviour
     [SerializeField] public float desiredseparation = 3f;    // Desired separation distance
     [SerializeField] public float neighbordist = 10f;    // Maximum distance to consider for neighbors
 
-    public List<Bird> getAllBirds(){
+    public List<Bird> getAllBirds()
+    {
         return birds;
     }
 
-    public void AddBird(Bird b){
+    public void AddBird(Bird b)
+    {
         birds.Add(b);
     }
 
-    void Awake(){
+    void Awake()
+    {
         birds = new List<Bird>();
     }
     // Start is called before the first frame update
     void Start()
     {
+        SpawnBirds();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SpawnBirds()
+    {
+        if (birdPrefab)
+        {
+            for (int i = 0; i < numberOfBirds; i++)
+            {
+                // Spawn bird
+                GameObject newBird = Instantiate(
+                    birdPrefab, 
+                    new Vector3(Random.Range(-boxSize, boxSize), 
+                    Random.Range(-boxSize, boxSize), 0), 
+                    Quaternion.identity
+                );
+
+                // Set parent to flock
+                newBird.transform.parent = transform;
+
+                // Add bird to list
+                birds.Add(newBird.GetComponent<Bird>());
+            }
+        }
     }
 }
